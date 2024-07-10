b9e4196055e82a5c449be51716e01d3152991fcf
yevhenii <glogatskiy@gmail.com>
2024-07-10 19:11:07 +0300

    FarAd Notifier Bot 1.0.1

--- functions/notifier.js
+++ functions/notifier.js
@@ -1,45 +1,6 @@
-require('dotenv').config();
+const { CORS_HEADERS } = require('../config');
 
-const { CONTRACT_ABI, CORS_HEADERS } = require('../config');
-const { NeynarAPIClient } = require('@neynar/nodejs-sdk');
-const { ethers } = require('ethers');
-
-const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY);
-const provider = new ethers.JsonRpcProvider(process.env.MAINNET_ENDPOINT);
-const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS, CONTRACT_ABI, provider);
-
-const handleEvent = async (offerId, offerer, receiver, price, token, duration, castURL, castType, event) => {
-    console.log(`Event caught: offerId=${offerId}, offerer=${offerer}, receiver=${receiver}, price=${price.toString()}, token=${token}, duration=${duration.toString()}, castURL=${castURL}, castType=${castType}`);
-
-    try {
-        if (!process.env.SIGNER_UUID) {
-            throw new Error("SIGNER_UUID is not provided");
-        }
-
-        const receiverData = await client.lookupUserByCustodyAddress(receiver);
-
-        try {
-            const cast = await client.publishCast(
-                process.env.SIGNER_UUID,
-                `Hey @${receiverData.user.username}! You just received an ad offer, check it out here: ${process.env.OFFERS_PAGE_URL}`
-            );
-
-            if (cast) {
-                console.log(JSON.stringify(cast));
-            }
-        } catch {
-            console.error()
-        }
-
-        return new Response();
-    } catch (e) {
-        return new Response(e.message, { status: 500 });
-    }
-};
-
-contract.on('NewOfferCreated', handleEvent);
-
-exports.handler = async function(event, context) {
+exports.handler = async function() {
     return {
         statusCode: 200,
         headers: CORS_HEADERS,
