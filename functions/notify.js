a4b395b5b824999a5b2af2f0c9bf372cd9e3fe22
yevhenii <glogatskiy@gmail.com>
2024-08-05 15:45:37 +0300

    Notifier Bot 1.0.4.8

--- functions/notify.js
+++ functions/notify.js
@@ -13,7 +13,7 @@ exports.handler = async function(event, context, callback) {
 
     const resp = JSON.parse(event.body);
 
-    if (resp.abi.size === 0 && resp.logs.size === 0 && !resp.confirmed) {
+    if (!resp.confirmed || resp.abi.size === 0 && resp.logs.size === 0) {
         return {
             statusCode: 200,
             headers: CORS_HEADERS,
