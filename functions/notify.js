dfe18b5ea82ae192ac4323c006e171a46ec67174
yevhenii <glogatskiy@gmail.com>
2024-08-15 15:48:12 +0300

    Notifier Bot 1.0.4.14

--- functions/notify.js
+++ functions/notify.js
@@ -10,12 +10,13 @@ const contractInterface = new ethers.Interface(JSON.stringify(ABI_FRAGMENT));
 
 exports.handler = async function(event, context, callback) {
     const resp = JSON.parse(event.body);
+    console.log(event.body);
 
     if ((resp.abi.size === 0 && resp.logs.size === 0) || !resp.confirmed) {
         return {
             statusCode: 200,
             headers: CORS_HEADERS,
-            body: JSON.stringify({ message: 'Test Webhook' }),
+            body: JSON.stringify({ message: 'Empty' }),
         };
     }
 
